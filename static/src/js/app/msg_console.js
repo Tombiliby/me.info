1d1b098442d9e745d6e6de774e2fd7ca178d1688
Tombiliby <tombiliby@gmail.com>
2015-06-30 08:41:26 +0200

    New photo

--- static/src/js/app/msg_console.js
+++ static/src/js/app/msg_console.js
@@ -12,7 +12,7 @@ var consoleStyle_body = [
     , "color: #333"
 ].join(';');
 
-console.log("%cHey you !!", consoleStyle_title);
+console.log("%cHey hey,", consoleStyle_title);
 console.log("%cVous semblez bien curieux du front-end de mon CV.\nLes sources, si vous le souhaitez, sont ici : https://github.com/Tombiliby/me.info", consoleStyle_body);
 console.log("\n");
 console.log("%cPour toute autre information n'hésitez pas à me contacter", consoleStyle_body);
