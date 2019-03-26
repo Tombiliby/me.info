adb5d8d0c2ad5e028d74c983f8e29fe7cefd2355
thomas.champion <thomas.champion@orchestra.eu>
2019-03-26 17:57:55 +0100

    New Theme and dark mode added at 9h pm

--- static/src/js/app/msgHello.js
+++ static/src/js/app/msgHello.js
@@ -1,5 +1,8 @@
 var now = new Date();
 var hours = now.getHours();
-if (hours > 18) {
+if (hours > 17) {
 	$("#msg_hello").text("BONSOIR");
+}
+if (hours > 20) {
+	$('head').append('<link rel="stylesheet" href="./static/dist/css/style_dark.css" type="text/css" />');
 }
--- static/src/js/app/msg_console.js
+++ static/src/js/app/msg_console.js
@@ -1,23 +1,19 @@
 var consoleStyle_title = [
 		"font-size: 16px"
-		, "font-family: 'SF UI text', Helvetica, Arial, sans-serif"
+		, "font-family: Helvetica, Arial, sans-serif"
     , "font-weight: bold"
     , "line-height: 22px"
-    , "color: #929292"
+    , "color: #333"
 ].join(';');
 
 var consoleStyle_body = [
-		"font-size: 12px"
-    , "font-family: 'SF UI text', Helvetica, Arial, sans-serif"
-    , "color: #929292"
-].join(';');
-
-var fx_body = [
 		"font-size: 14px"
-    , "background-color: #252525"
-    , "color: #2af598"
+    , "font-family: Helvetica, Arial, sans-serif"
+    , "color: #333"
 ].join(';');
 
-console.log("%c                       \n   +-+-+-+-+-+-+-+-+   \n   |C|H|A|M|P|I|O|N|   \n   +-+-+-+-+-+-+-+-+   \n                       ", fx_body);
-console.log("%cLes sources, si vous le souhaitez, sont ici : https://github.com/Tombiliby/me.info\nPour toute autre information n'hésitez pas à me contacter", consoleStyle_body);
+console.log("%cHey you !!", consoleStyle_title);
+console.log("%cVous semblez bien curieux du front-end de mon CV.\nLes sources, si vous le souhaitez, sont ici : https://github.com/Tombiliby/me.info", consoleStyle_body);
+console.log("\n");
+console.log("%cPour toute autre information n'hésitez pas à me contacter", consoleStyle_body);
 
